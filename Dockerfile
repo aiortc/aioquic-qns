894ebdd1b50c5c90628627e2708b9e5dc3d7dfda
Jeremy Lainé <jeremy.laine@m4x.org>
2020-02-10 01:29:37 +0100

    start adding support for interop runner

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM martenseemann/quic-network-simulator-endpoint:latest
 
 RUN apt-get update
 RUN apt-get install -y git-core libssl-dev python3-dev python3-pip
-RUN git clone https://github.com/aiortc/aioquic
+RUN pip3 install aiofiles asgiref httpbin starlette wsproto
+RUN git clone https://github.com/aiortc/aioquic && cd /aioquic && git checkout ba1447d8ed9696a9f155d72106b8c2b9d708a9ee
 WORKDIR /aioquic
 RUN pip3 install -e .
-RUN pip3 install aiofiles asgiref httpbin starlette wsproto
 
 COPY run_endpoint.sh .
 RUN chmod +x run_endpoint.sh
--- run_endpoint.sh
+++ run_endpoint.sh
@@ -8,6 +8,27 @@
 # - SERVER_PARAMS contains user-supplied command line parameters
 # - CLIENT_PARAMS contains user-supplied command line parameters
 
+if [ -n "${TESTCASE}" ]; then
+    # interop runner
+    case "${TESTCASE}" in
+        "handshake")
+            CLIENT_PARAMS="--legacy-http"
+            ;;
+        "http3")
+            ;;
+        "retry")
+            CLIENT_PARAMS="--legacy-http"
+            SERVER_PARAMS="--stateless-retry"
+            ;;
+        *)
+            exit 127
+            ;;
+    esac
+else
+    # network simulator
+    REQUESTS="https://server/1000000"
+fi
+
 if [ "$ROLE" = "client" ]; then
     # Wait for the simulator to start up.
     /wait-for-it.sh sim:57832 -s -t 30
@@ -16,7 +37,8 @@ if [ "$ROLE" = "client" ]; then
         --insecure \
         --secrets-log /logs/ssl.log \
         --verbose \
-        https://server/1000000
+        $CLIENT_PARAMS \
+        $REQUESTS
 elif [ "$ROLE" = "server" ]; then
     echo "Starting server"
     python3 examples/http3_server.py \
@@ -24,5 +46,6 @@ elif [ "$ROLE" = "server" ]; then
         --port 443 \
         --private-key tests/ssl_key.pem \
         --secrets-log /logs/ssl.log \
-        --verbose
+        --verbose \
+        $SERVER_PARAMS
 fi
